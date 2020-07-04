85b9407507abe0b6eb3408a8707fa45694cfb799
GrenBro <67783950+GrenBro@users.noreply.github.com>
2020-07-04 14:19:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-	'NAME BOT': 'greneco',
+	'TOKEN': 'NzI0NjEyNzk0MTAzODg5OTYw.XwBiwQ.C_E2qHjtwULGEF5ahock3VGz6ZI',
+	"NAME BOT": 'Gren_eco',
 	'ID': 723987130287652988,
 	'PREFIX': '.'
-}
