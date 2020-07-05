190be694ca90549d1ff0893b749707dedc20c4c3
GrenBro <67783950+GrenBro@users.noreply.github.com>
2020-07-05 15:32:51 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -50,10 +50,10 @@ async def on_ready():
 for guild in client.guilds:
 		 for member in guild.members:
 			 if cursor.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is None:
-				 cursor.execute(f"INSERT INTO users VALUES ('{member}', {member.id}, 0, 0, 0)")
-				 connection.commit()
-				 print('Bot connected')
-@client.event
+			 	cursor.execute(f"INSERT INTO users VALUES ('{member}', {member.id}, 0, 0, 0)")
+			 	connection.commit()
+			 	print('Bot connected')
+
 async def on_member_join(member):
 	if cursor.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is None:
 		cursor.execute(f"INSERT INTO users VALUES ('{member}', {member.id}, 0, 0, 1)")
